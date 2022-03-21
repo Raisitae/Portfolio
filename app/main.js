975e5dee59d7a49ffc8699865b5009614c119100
Raisa <raisaerlich@gmail.com>
2022-03-21 19:56:46 -0300

    js and basic contact form structure

---
+++ app/main.js
@@ -0,0 +1,64 @@
+let form = document.getElementById("draggable");
+
+let btnContact = document.getElementById("contacto");
+
+let btnCloseForm = document.getElementById("close-form");
+
+btnContact.addEventListener("click", openForm);
+
+btnCloseForm.addEventListener("click", closeForm);
+
+function openForm() {
+  form.style.display = "flex";
+}
+
+function closeForm() {
+  form.style.display = "none";
+}
+
+// Make the DIV element draggable:
+dragElement(document.getElementById("draggable"));
+
+function dragElement(elmnt) {
+  let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById("draggable-bar")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById("draggable-bar").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
